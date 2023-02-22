a1719c3d1fc5d56f255905947617f18215699519
Rivaldo Ramos <ramosmendesfilho@hotmail.com>
2023-02-21 22:38:39 -0300

    Updated project

--- InternalRevenueService/src/entities/LegalPerson.java
+++ InternalRevenueService/src/entities/LegalPerson.java
@@ -2,4 +2,5 @@ package entities;
 
 public class LegalPerson {
 
+	private Integer numberOfEmployees;
 }
--- InternalRevenueService/src/entities/NaturalPerson.java
+++ InternalRevenueService/src/entities/NaturalPerson.java
@@ -1,5 +1,30 @@
 package entities;
 
-public class NaturalPerson {
+public class NaturalPerson extends Person {
+
+	private Double healthExpenses;
+
+	public NaturalPerson(String name, Double annualIncome, Double healthExpenses) {
+		super(name, annualIncome);
+		this.healthExpenses = healthExpenses;
+	}
+
+	public Double getHealthExpenses() {
+		return healthExpenses;
+	}
+
+	@Override
+	public Double taxes() {
+		double health;
+		if (healthExpenses != 0.00 || healthExpenses != null)
+			health = 0.00;
+		else
+			health = (healthExpenses * 0.5);
+
+		if (getAnnualIncome() < 20000.00)
+			return (getAnnualIncome() * 0.15) - health;
+		else
+			return (getAnnualIncome() * 0.25) - health;
+	}
 
 }
